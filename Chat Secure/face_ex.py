c2f23c5f7b4130c6426b7a470f999ec705bab7d6
Alexander Seelye <alexander.seelye@gmail.com>
2023-12-03 21:33:59 -0600

    custom nao module

---
+++ Chat Secure/face_ex.py
@@ -0,0 +1,144 @@
+# -*- encoding: UTF-8 -*-
+""" Say 'hello, you' each time a human face is detected
+
+"""
+
+import sys
+import time
+
+from naoqi import ALProxy
+from naoqi import ALBroker
+from naoqi import ALModule
+from PIL import Image
+
+from optparse import OptionParser
+
+NAO_IP = "192.168.40.170"
+
+
+# Global variable to store the HumanGreeter module instance
+HumanGreeter = None
+memory = None
+
+
+class HumanGreeterModule(ALModule):
+    """ A simple module able to react
+    to facedetection events
+
+    """
+    def __init__(self, name):
+        ALModule.__init__(self, name)
+        # No need for IP and port here because
+        # we have our Python broker connected to NAOqi broker
+
+        # Create a proxy to ALTextToSpeech for later use
+        self.tts = ALProxy("ALTextToSpeech")
+        self.camProxy = ALProxy("ALVideoDevice")
+
+        # Subscribe to the FaceDetected event:
+        global memory
+        memory = ALProxy("ALMemory")
+        memory.subscribeToEvent("FaceDetected",
+            "HumanGreeter",
+            "onFaceDetected")
+
+    def onFaceDetected(self, *_args):
+        """ This will be called each time a face is
+        detected.
+
+        """
+        # Unsubscribe to the event when talking,
+        # to avoid repetitions
+        memory.unsubscribeToEvent("FaceDetected",
+            "HumanGreeter")
+
+        self.tts.say("Hello!")
+        resolution = 2    # VGA
+        colorSpace = 11   # RGB
+
+        videoClient = self.camProxy.subscribe("python_client", resolution, colorSpace, 5)
+
+        t0 = time.time()
+
+        # Get a camera image.
+        # image[6] contains the image data passed as an array of ASCII chars.
+        naoImage = self.camProxy.getImageRemote(videoClient)
+
+        t1 = time.time()
+
+        # Time the image transfer.
+        print ("acquisition delay ", t1 - t0)
+
+        self.camProxy.unsubscribe(videoClient)
+
+
+        # Now we work with the image returned and save it as a PNG  using ImageDraw
+        # package.
+
+        # Get the image size and pixel array.
+        imageWidth = naoImage[0]
+        imageHeight = naoImage[1]
+        array = naoImage[6]
+
+        # Create a PIL Image from our pixel array.
+        im = Image.frombytes("RGB", (imageWidth, imageHeight), array)
+
+        # Save the image.
+        im.save("camImage.png", "PNG")
+
+        im.show()
+
+        # Subscribe again to the event
+        memory.subscribeToEvent("FaceDetected",
+            "HumanGreeter",
+            "onFaceDetected")
+
+
+def main():
+    """ Main entry point
+
+    """
+    parser = OptionParser()
+    parser.add_option("--pip",
+        help="Parent broker port. The IP address or your robot",
+        dest="pip")
+    parser.add_option("--pport",
+        help="Parent broker port. The port NAOqi is listening to",
+        dest="pport",
+        type="int")
+    parser.set_defaults(
+        pip=NAO_IP,
+        pport=9559)
+
+    (opts, args_) = parser.parse_args()
+    pip   = opts.pip
+    pport = opts.pport
+
+    # We need this broker to be able to construct
+    # NAOqi modules and subscribe to other modules
+    # The broker must stay alive until the program exists
+    myBroker = ALBroker("myBroker",
+       "0.0.0.0",   # listen to anyone
+       0,           # find a free port and use it
+       pip,         # parent broker IP
+       pport)       # parent broker port
+
+
+    # Warning: HumanGreeter must be a global variable
+    # The name given to the constructor must be the name of the
+    # variable
+    global HumanGreeter
+    HumanGreeter = HumanGreeterModule("HumanGreeter")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print ("Interrupted by user, shutting down")
+        myBroker.shutdown()
+        sys.exit(0)
+
+
+
+if __name__ == "__main__":
+    main()
